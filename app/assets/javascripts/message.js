d8cf517a5bed45cebdf3c89fae7ede64e60178ca
toshinari-ishii <toshi1225m@gmail.com>
2020-01-10 16:57:09 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,6 @@ $('#new_message').on('submit', function(e){
     $('.messages').append(html);      
        $('form')[0].reset();
        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      //  $('.form__submit').prop('disabled', false);
   })
   .fail(function(){
     alert('メッセージ送信に失敗しました');
