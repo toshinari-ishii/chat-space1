67b5e4f2327168b74257ebc6bcdd1533493fcb35
toshinari-ishii <toshi1225m@gmail.com>
2020-01-15 14:41:34 +0900

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'toshinarichatspaceimage'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/toshinarichatspaceimagevim ~/.zshrc'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/toshinarichatspaceimage'
 end
